97d4c113a98b9ccf6969482a7a30847057c0543b
yoni1994 <yoniklein1994@gmail.com>
2021-06-11 16:54:02 -0500

    started working on getGuess

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
+    prevGuesses: [],
     play: function() {
-      this.secretNum = Math.floor(Math.random() * 
+        this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    }
+    },
+    getGuess: function() {
+        let guess = window.prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
+        guess = parseInt(guess)
+        while (typeof guess !== 'number' || guess === NaN) {
+            guess = window.prompt(`Please try again. Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+        }
+        console.log(guess)
+    },
   }
+
+game.getGuess()
