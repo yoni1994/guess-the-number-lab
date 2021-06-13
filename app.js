3bd60849a18ca1c921f1c559889877c9fd9e24e2
yoni1994 <yoniklein1994@gmail.com>
2021-06-13 12:32:52 -0500

    make a setRange function, call getGuess, and start making render

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 const game = {
     title: 'Guess the Number!',
-    biggestNum: 100,
-    smallestNum: 1,
+    biggestNum: null,
+    smallestNum: null,
     secretNum: null,
     prevGuesses: [],
     play: function() {
-        this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+        this.setRange()
+        this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+        do {
+            this.prevGuesses.push(this.getGuess())
+
+        } 
+        while(this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
+        alert('you won')
     },
     getGuess: function() {
-        let guess = window.prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
-        guess = parseInt(guess)
-        while (typeof guess !== 'number' || guess === NaN) {
-            guess = window.prompt(`Please try again. Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+        let guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`))
+        while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
+            guess = prompt(`Please try again. Enter a number between ${this.smallestNum} and ${this.biggestNum}.`)
         }
-        console.log(guess)
+        return guess
     },
+    setRange: function() {
+        while (this.smallestNum === null || isNaN(this.smallestNum)) {
+            this.smallestNum = parseInt(prompt('Pleae enter a number. This will be the bottom of the guessing range.'))
+        }
+        while (this.biggestNum === null || isNaN(this.biggestNum) || this.biggestNum < this.smallestNum + 2) {
+            this.biggestNum = parseInt(prompt(`Please enter a number equal to or higher than ${this.smallestNum + 2}. This will be the top of the guessing range.`))
+        }
+    },
+    render: function() {
+        if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
+            alert(`Congrats! You guessed the number in ${this.prevGuesses.length} ${this.prevGuesses.length = 1 ? 'guess' : 'guesses'}`)
+        }
+    }
   }
 
-game.getGuess()
+game.play()
